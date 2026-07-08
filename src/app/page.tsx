a00609578c15a2558fcaa1a5ff6f2911a3cd2a50
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-08 21:14:26 +0330

    feat: Creat non-dashboard page navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+
 export default function Home() {
-	return <div>Hello Again</div>;
+	return (
+		<div className="nondashboard-layout">
+			<NonDashboardNavbar />
+		</div>
+	);
 }
