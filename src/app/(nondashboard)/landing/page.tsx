59224245ba800f2060007bd74e8eb21052838e79
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-09 14:27:16 +0330

    feat: Add next.config.js and show Courses on Landing page

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import CourseCardSearch from "@/components/CourseCardSearch";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useCarousel } from "@/hooks/useCarousel";
 import { useGetCoursesQuery } from "@/state/api";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import CourseCardSearch from "@/components/CourseCardSearch";
 
 const LoadingSkeleton = () => {
 	return (
@@ -42,8 +43,13 @@ const LoadingSkeleton = () => {
 };
 
 export default function Landing() {
+	const router = useRouter();
 	const currentImage = useCarousel({ totalImages: 3 });
 	const { data: courses, isLoading, isError } = useGetCoursesQuery({});
+
+	const handleCourseClick = (courseId: string) => {
+		router.push(`/search?id=${courseId}`);
+	};
 
 	return (
 		<motion.div
@@ -126,7 +132,10 @@ export default function Landing() {
 								transition={{ duration: 0.5, delay: index * 0.2 }}
 								viewport={{ amount: 0.4 }}
 							>
-								<CourseCardSearch course={course} />
+								<CourseCardSearch
+									course={course}
+									onClick={() => handleCourseClick(course.courseId)}
+								/>
 							</motion.div>
 						))}
 				</div>
