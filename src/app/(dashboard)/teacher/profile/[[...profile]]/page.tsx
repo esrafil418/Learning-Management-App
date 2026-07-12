6a94b94fbbddc09138d52c7b2500d377ac847285
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-12 15:09:20 +0330

    feat: Add dashboard Header into teacher profile page

---
+++ src/app/(dashboard)/teacher/profile/[[...profile]]/page.tsx
@@ -0,0 +1,26 @@
+import { UserProfile } from "@clerk/nextjs";
+import Header from "@/components/Header";
+import { dark } from "@clerk/themes";
+
+export default function UserProfilePage() {
+	return (
+		<>
+			<Header title="Profile" subtitle="View your Profile" />
+			<UserProfile
+				path="/teacher/profile"
+				routing="path"
+				appearance={{
+					baseTheme: dark,
+					elements: {
+						scrollBox: "bg-customgreys-darkGrey",
+						navbar: {
+							"& > div:nth-child(1)": {
+								background: "none",
+							},
+						},
+					},
+				}}
+			/>
+		</>
+	);
+}
